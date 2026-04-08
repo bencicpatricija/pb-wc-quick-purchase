d620fb87a88b78904c97e4165b432d8d1f812b50
Patricija <bencicpatricija@gmail.com>
2026-04-09 01:45:01 +0200

    Add admin checkbox control for quick purchase

--- pb-wc-quick-purchase.php
+++ pb-wc-quick-purchase.php
@@ -23,3 +23,27 @@ if ( ! defined( 'WPINC' ) ) {
 // Define plugin constants.
 define( 'PB_WC_QUICK_PURCHASE_VERSION', '1.0.0' );
 define( 'PB_WC_QUICK_PURCHASE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+// Add a checkbox to the product edit page to enable/disable quick purchase for each product.
+add_action(
+  'woocommerce_product_options_general_product_data',
+  function () {
+    woocommerce_wp_checkbox(
+      [
+        'id'          => '_pb_wcqp_enabled',
+        'label'       => __( 'Quick purchase', 'pb-wc-quick-purchase' ),
+        'description' => __( 'Display a "Quick purchase" button on the product page that skips the cart and goes straight to checkout.', 'pb-wc-quick-purchase' ),
+      ]
+    );
+  }
+);
+
+// Save the checkbox value when the product is saved.
+add_action(
+  'woocommerce_process_product_meta',
+  function ( $post_id ) {
+    $enabled = isset( $_POST['_pb_wcqp_enabled'] ) ? 'yes' : 'no';
+
+    update_post_meta( $post_id, '_pb_wcqp_enabled', $enabled );
+  }
+);
